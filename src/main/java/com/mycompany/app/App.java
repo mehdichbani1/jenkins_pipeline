a26910d16a018f3361359db83f4803708ab05fd2
Mahmoud El Mehdi Chbani <114113455+mehdichbani1@users.noreply.github.com>
2023-05-07 20:30:02 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hell World!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
